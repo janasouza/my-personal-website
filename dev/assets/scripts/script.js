dca0f9f219cb15d42390a787679f6d52e3d76b1c
afonsopacifer <afonsopolis@gmail.com>
2016-02-23 12:49:55 -0300

    Feedback adicionado no menu

--- dev/assets/scripts/script.js
+++ dev/assets/scripts/script.js
@@ -1,1 +1,23 @@
-console.log("ok");
+// Menu feedback
+$(document).ready(function(){
+
+  var navLink = $('.menu-link'),
+     local = window.location.pathname.split('/').pop();
+
+  menuFeedback();
+
+  function menuFeedback() {
+    navLink.each(function(){
+      if ($(this).attr('href') == local ) {
+        $(this).addClass('active');
+      } else {
+        $(this).removeClass('active');
+      }
+    });
+  };
+
+  if(local == "") {
+    $('.menu-link:first').addClass('active');
+  }
+
+});
